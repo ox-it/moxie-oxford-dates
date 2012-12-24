a69c8f189561491452057343ff1196bb5db62dbb
Martin Filliau <martin@filliau.com>
2012-12-24 16:30:08 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+moxie-oxford-dates
+==================
+
+Moxie application to provide term dates in various formats for University of Oxford.
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+install_requires = open('requirements.txt').readlines()
+
+setup(name='moxie-oxford-dates',
+    version='0.1',
+    packages=find_packages(),
+    description='Oxford dates module for Moxie',
+    author='Mobile Oxford',
+    author_email='mobileoxford@oucs.ox.ac.uk',
+    url='https://github.com/ox-it/moxie-oxford-dates',
+    include_package_data=True,
+    setup_requires=["setuptools"],
+    install_requires=install_requires,
+    test_suite="moxie_oxford_dates.tests",
+)
