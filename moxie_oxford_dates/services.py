c3ae4f261bd6a652e231b39779f16e9a83f2a6f3
Martin Filliau <martin@filliau.com>
2012-12-24 17:13:41 +0100

    Added components of today's date as per TRELLO#61.

--- moxie_oxford_dates/services.py
+++ moxie_oxford_dates/services.py
@@ -12,3 +12,8 @@ class DatesService(Service):
         """Returns today's date with the name of the term.
         """
         return oxford_term_dates.format_today()
+
+    def get_today_components(self):
+        """Returns today's date components (week number, term short/long)
+        """
+        return oxford_term_dates.ox_date_dict()
--- moxie_oxford_dates/views.py
+++ moxie_oxford_dates/views.py
@@ -9,6 +9,7 @@ class Today(ServiceView):
     def handle_request(self):
         dates_service = DatesService.from_context()
         response = {
-            'today': dates_service.get_today_date()
+            'today': dates_service.get_today_date(),
+            'components': dates_service.get_today_components()
         }
         return response
