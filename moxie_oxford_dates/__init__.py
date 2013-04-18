e525c2860b03a39ab03601025c4c46eaabc05d7c
David King <dave@davbo.org>
2013-04-18 16:02:56 +0100

    Pass blueprint configuration through on init

--- moxie_oxford_dates/__init__.py
+++ moxie_oxford_dates/__init__.py
@@ -3,8 +3,8 @@ from flask import Blueprint
 from .views import Today
 
 
-def create_blueprint(blueprint_name):
-    oxford_dates_blueprint = Blueprint(blueprint_name, __name__)
+def create_blueprint(blueprint_name, conf):
+    oxford_dates_blueprint = Blueprint(blueprint_name, __name__, **conf)
 
     oxford_dates_blueprint.add_url_rule('/today',
             view_func=Today.as_view('today'))
