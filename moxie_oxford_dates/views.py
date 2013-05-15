b970412b1644c58a6993985ec8a157e4c82c5fc1
Martin Filliau <martin@filliau.com>
2013-05-15 14:28:06 +0100

    Handle client errors

--- moxie_oxford_dates/views.py
+++ moxie_oxford_dates/views.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from flask import request, url_for
 
 from moxie.core.views import ServiceView
+from moxie.core.exceptions import abort
 from .services import DatesService 
 
 
@@ -28,7 +29,11 @@ class Date(ServiceView):
 
     def handle_request(self, year, month, day):
         dates_service = DatesService.from_context()
-        components = dates_service.get_ox_components(datetime(int(year), int(month), int(day)))
+        try:
+            date = datetime(int(year), int(month), int(day))
+        except ValueError as ve:
+            return abort(400, ve.message)
+        components = dates_service.get_ox_components(date)
         return {
             'formatted': dates_service.get_formatted_date(components),
             'components': components,
